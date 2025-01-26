de7a8f6c7501c0ff995bb54d0d20d3112ba8dfcd
RicardoSilva94 <ricardojgsilva94@gmail.com>
2025-01-26 01:13:21 +0000

    Change profile card bg color

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -92,7 +92,7 @@ const Profile: React.FC = () => {
             <main className="flex-grow">
                 <Navbar />
                 <div className="container mx-auto p-6">
-                    <div className="bg-white shadow-md rounded-lg p-6">
+                    <div className="bg-amber-50 shadow-md rounded-lg p-6">
                         <div className="flex justify-between items-center mb-6">
                             <h1 className="text-2xl font-bold">Profile</h1>
                         </div>
